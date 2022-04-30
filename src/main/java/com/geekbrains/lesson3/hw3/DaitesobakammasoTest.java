d01fd497a2a060119c7cde13af6292a369740d4b
Svetlana <ssv-71@mail.ru>
2022-04-30 20:22:50 +0300

    add hw3

---
+++ src/main/java/com/geekbrains/lesson3/hw3/DaitesobakammasoTest.java
@@ -0,0 +1,40 @@
+package com.geekbrains.lesson3.hw3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DaitesobakammasoTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromiumdriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://daitesobakammaso.ru/");
+
+        WebElement directoryEntry = driver.findElement(By.xpath("//td[text()='В каталог']"));
+        directoryEntry.click();
+
+        driver.findElement(By.xpath("//div[text()='Акции и скидки']")).click();
+
+        driver.findElement(By.xpath("//div[text()='Бараний корень сушёный']")).click();
+
+        driver.findElement(By.xpath("//div[text()='Потрошки северного оленя']")).click();
+
+        driver.findElement(By.xpath("//div[@class='t706__carticon-wrapper']")).click();
+
+        driver.findElement(By.xpath("//input[@data-tilda-rule='name']")).sendKeys("Иван");
+
+        driver.findElement(By.xpath("//input[@data-tilda-rule='email']")).sendKeys("sdf@mail.ru");
+
+        driver.findElement(By.xpath("//input[@type='tel']")).sendKeys("+79897658765");
+
+        driver.findElement(By.xpath("//input[@name='Адрес']")).sendKeys("Москва, Советская, 32,2");
+
+        driver.findElement(By.xpath("//button[text()='Оформить заказ']")).click();
+
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
---
+++ src/main/java/com/geekbrains/lesson3/hw3/StudentlibraryTest.java
@@ -0,0 +1,42 @@
+package com.geekbrains.lesson3.hw3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StudentlibraryTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.studentlibrary.ru/");
+
+        driver.findElement(By.xpath("//div[@class='main-block wrap-catalogue-block']/descendant::span[text()='Каталог']")).click();
+
+        driver.findElement(By.xpath("//span[@class='item-head']")).click();
+//        driver.findElement(By.xpath("//span[text()='Медицинские науки']")).click();
+        driver.findElement(By.xpath("//a[@name='btn-med-on']")).click();
+
+        driver.findElement(By.xpath("//a[text()='Медицина (ВО) ГЭОТАР-Медиа. Базовый комплект']")).click();
+
+        driver.findElement(By.xpath("//input[@autocomplete='off']")).sendKeys("фармакология");
+
+        driver.findElement(By.xpath("//span[@class='input-group-btn']")).click();
+
+        driver.findElement(By.xpath("//a[@href='https://www.studentlibrary.ru/ru/book/06-COS-2401.html']")).click();
+
+        driver.findElement(By.xpath("//a[@id='a-to_first_chapter']")).click();
+
+        driver.findElement(By.xpath("//span[@class='substr-img']")).click();
+
+        Cookie cookie = new Cookie("rdsulg", "kmm_2002");
+        driver.manage().addCookie(cookie);
+        driver.navigate().refresh();
+
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+}
